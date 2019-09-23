758ab5892c33e8622a135790d3e0e8c098fb8572
Raphael Oliveira <raphaelfogaca@hotmail.com>
2019-09-22 22:18:02 -0300

    alterações boleto no financeiro.cs

--- ControleRotasMvc/ControleRotasMvc/Controllers/FinanceiroController.cs
+++ ControleRotasMvc/ControleRotasMvc/Controllers/FinanceiroController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace ControleRotasMvc.Controllers
 {
@@ -14,13 +15,13 @@ namespace ControleRotasMvc.Controllers
         //GET: Mensalidade
         public ActionResult Index()
         {
+                   
             FinanceiroEntity dao = new FinanceiroEntity();
             IList<FinanceiroViewModel> docfin = dao.DocumentoFinanceiro();
 
             AlunoEntity dbAluno = new AlunoEntity();
             IQueryable<Aluno> aluno = dbAluno.Alunos();
             ViewBag.Aluno = aluno;
-
 
             return View(docfin);
         }
@@ -49,6 +50,15 @@ namespace ControleRotasMvc.Controllers
             FinanceiroEntity dao = new FinanceiroEntity();
             Financeiro documento = dao.BuscarFinanceiroPorId(docfin.Id);
             documento.Situacao = 1;
+
+            Boleto boleto = new Boleto();
+            boleto = GerarBoleto(docfin.Id);
+            documento.BoletoBarcode = boleto.barcode;
+            documento.BoletoBarcode = boleto.barcode;
+            documento.BoletoCode = boleto.code;
+            documento.BoletoPaymentLink = boleto.paymentLink;
+            documento.BoletoVencimento = boleto.dueDate;
+
             dao.Alterar(documento);
             return RedirectToAction("Index");
         }
@@ -93,6 +103,28 @@ namespace ControleRotasMvc.Controllers
             ViewBag.Aluno = aluno;
 
             return View("Index", listaFinanceiro);
+        }
+
+        public static Boleto GerarBoleto(int id)
+        {
+            string resultado = "{\"boletos\":[{\"code\":\"EDA6DEA7-10CB-4AF9-9C6F-FFD965BE963F\",\"paymentLink\":\"https://pagseguro.uol.com.br/checkout/payment/booklet/print.jhtml?c=c391274ab4b4208fad7df340226c2b7a56cfc3281e69429ae1854a9ee3418219fd11631b151f97d2\",\"barcode\":\"03399853012970000035849932701011980190000003100\",\"dueDate\":\"2019-09-21\"}]}";
+            RootObject rootObject = JsonConvert.DeserializeObject<RootObject>(resultado);
+            Boleto boleto = rootObject.boletos.FirstOrDefault();
+            return boleto;                       
+        }
+        
+
+        public class Boleto
+        {
+            public string code { get; set; }
+            public string paymentLink { get; set; }
+            public string barcode { get; set; }
+            public string dueDate { get; set; }
+        }
+
+        public class RootObject
+        {
+            public List<Boleto> boletos { get; set; }
         }
     }
 }
